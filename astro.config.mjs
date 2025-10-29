e7ab33bc8332bc3f51b0b4def1f8889274c7d547
Ángel Baez <baezdev@gmail.com>
2025-10-29 15:25:50 -0600

    feat: update site configuration, enhance robots.txt, and add keywords meta tag for improved SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,8 +7,26 @@ import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://gm-abogados.com.mx/",
-  integrations: [react(), sitemap()],
+  site: "https://gm-abogados.com.mx",
+  integrations: [
+    react(),
+    sitemap({
+      // Personalización opcional
+      changefreq: "weekly",
+      priority: 1.0,
+      lastmod: new Date(),
+
+      // Incluir secciones con anchors
+      customPages: [
+        "https://gm-abogados.com.mx/",
+        "https://gm-abogados.com.mx/#do-you-indentify",
+        "https://gm-abogados.com.mx/#services",
+        "https://gm-abogados.com.mx/#faq",
+        "https://gm-abogados.com.mx/#success-stories",
+        "https://gm-abogados.com.mx/#contact",
+      ],
+    }),
+  ],
   output: "server",
   adapter: netlify(),
 });
